990369c7c9bd39ca79584db9fb2ab9af9574fc87
Matthew John Ponce de Leon <mattpdl@fb.com>
2021-07-06 17:00:34 -0700

    Implement backend login and sign up functionality

---
+++ Instagram/LoginViewController.m
@@ -0,0 +1,98 @@
+//
+//  LoginViewController.m
+//  Instagram
+//
+//  Created by mattpdl on 7/6/21.
+//
+
+#import "LoginViewController.h"
+#import "Parse/Parse.h"
+
+@interface LoginViewController ()
+
+@end
+
+@implementation LoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+
+}
+
+- (void)loginUser {
+    // Get entered user credentials
+    NSString *username = self.usernameField.text;
+    NSString *password = self.passwordField.text;
+    
+    // Call login function with given credentials
+    [PFUser logInWithUsernameInBackground:username password:password block:^(PFUser * user, NSError *  error) {
+        if (error != nil) {
+            NSLog(@"User log in failed: %@", error.localizedDescription);
+            [self displayAlert:@"Login Error" withMessage:@"Please check your user credentials and internet connection and try again."];
+        } else {
+            NSLog(@"User logged in successfully");
+            
+            // display view controller that needs to shown after successful login
+        }
+    }];
+}
+
+- (void)registerUser {
+    // Initialize a user object
+    PFUser *newUser = [PFUser user];
+    [self setUserCredentials:newUser];
+    
+    // Call sign up function on the object
+    [newUser signUpInBackgroundWithBlock:^(BOOL succeeded, NSError * error) {
+        if (error != nil) {
+            NSLog(@"Error: %@", error.localizedDescription);
+            [self displayAlert:@"User Registration Error"
+                withMessage:@"Your user credentials could not be registered. This likely means your selected username is taken, or you do not have an internet connnection."];
+        } else {
+            NSLog(@"User registered successfully");
+            
+            // manually segue to logged in view
+        }
+    }];
+}
+
+- (void)setUserCredentials:(PFUser *)newUser {
+    if ([self.usernameField.text isEqualToString:@""]) {
+        [self displayAlert:@"Empty Username" withMessage:@"Your username cannot be empty."];
+        return;
+    } else if ([self.passwordField.text isEqualToString:@""]) {
+        [self displayAlert:@"Empty Password" withMessage:@"Your password cannot be empty."];
+        return;
+    }
+    
+    // Set user properties
+    newUser.username = self.usernameField.text;
+    newUser.password = self.passwordField.text;
+}
+
+- (void)displayAlert:(NSString *)title withMessage:(NSString *)msg {
+    // Create a UIAlertController object
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:title
+                               message:msg
+                               preferredStyle:(UIAlertControllerStyleAlert)];
+    
+    // Create a dismiss action
+    UIAlertAction *dismissAction = [UIAlertAction actionWithTitle:@"Dismiss" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {}];
+    [alert addAction:dismissAction];
+    
+    // Display alert
+    [self presentViewController:alert animated:YES completion:^{}];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
