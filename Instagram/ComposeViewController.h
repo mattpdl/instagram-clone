4aa58e77fb1014fc68e7b564579ff71a1e1add96
Matthew John Ponce de Leon <mattpdl@fb.com>
2021-07-08 13:45:23 -0700

    Begin work on resizing constraints for user images

--- Instagram/ComposeViewController.h
+++ Instagram/ComposeViewController.h
@@ -9,7 +9,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface ComposeViewController : UIViewController <UITableViewDelegate, UITableViewDataSource, UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+@interface ComposeViewController : UIViewController <UITableViewDelegate, UITableViewDataSource, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextViewDelegate>
 
 @end
 
--- Instagram/ComposeViewController.m
+++ Instagram/ComposeViewController.m
@@ -8,7 +8,7 @@
 #import "ComposeViewController.h"
 #import "Post.h"
 
-@interface ComposeViewController () <UITextViewDelegate>
+@interface ComposeViewController ()
 
 @property (weak, nonatomic) IBOutlet UIImageView *postImageView;
 @property (weak, nonatomic) IBOutlet UITextView *captionTextView;
@@ -24,20 +24,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    UIImagePickerController *imagePickerVC = [UIImagePickerController new];
-    imagePickerVC.delegate = self;
-    imagePickerVC.allowsEditing = YES;
-
-    // The Xcode simulator does not support taking pictures, so let's first check that the camera is indeed supported on the device before trying to present it.
-    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
-        imagePickerVC.sourceType = UIImagePickerControllerSourceTypeCamera;
-    }
-    else {
-        NSLog(@"Camera 🚫 available so we will use photo library instead");
-        imagePickerVC.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
-    }
-
-    [self presentViewController:imagePickerVC animated:YES completion:nil];
+    self.captionTextView.delegate = self;
 }
 
 - (IBAction)didTapCancel:(id)sender {
@@ -56,23 +43,76 @@
     }];
 }
 
+- (IBAction)didTapAdd:(id)sender {
+    UIImagePickerController *imagePickerVC = [UIImagePickerController new];
+    imagePickerVC.delegate = self;
+    imagePickerVC.allowsEditing = YES;
+
+    // The Xcode simulator does not support taking pictures, so let's first check that the camera is indeed supported on the device before trying to present it.
+    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+        imagePickerVC.sourceType = UIImagePickerControllerSourceTypeCamera;
+    }
+    else {
+        NSLog(@"Camera 🚫 available so we will use photo library instead");
+        imagePickerVC.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    }
+    
+    // Display image picker
+    [self presentViewController:imagePickerVC animated:YES completion:nil];
+}
+
+- (UIImage *)resizeImage:(UIImage *)image withSize:(CGSize)size {
+    UIImageView *resizeImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, size.width, size.height)];
+    
+    resizeImageView.contentMode = UIViewContentModeScaleAspectFill;
+    resizeImageView.image = image;
+    
+    UIGraphicsBeginImageContext(size);
+    [resizeImageView.layer renderInContext:UIGraphicsGetCurrentContext()];
+    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return newImage;
+}
+
 - (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info {
     
     // Get the image captured by the UIImagePickerController
     UIImage *originalImage = info[UIImagePickerControllerOriginalImage];
     UIImage *editedImage = info[UIImagePickerControllerEditedImage];
-    self.userImage = editedImage ? editedImage : originalImage;
+    UIImage *userImage = editedImage ? editedImage : originalImage;
+    
+    // Resize selected image
+    CGFloat height = userImage.size.height;
+    CGFloat width = userImage.size.width;
+    
+    if (height > 4000 || width > 4000) {
+        CGFloat maxDimension = MAX(height, width);
+        CGFloat aspectRatio = height / width;
+        
+        if (maxDimension == height) {
+            width = width * aspectRatio;
+        } else {
+            height = height / aspectRatio;
+        }
+    }
+    
+    // TODO: resize and set self.userImage
+    self.userImage = userImage;
     
     // Dismiss UIImagePickerController to go back to your original view controller
-    [self dismissViewControllerAnimated:YES completion:nil];
+    [self dismissViewControllerAnimated:YES completion:^{
+        // Set image preview in compose view controller
+        [self.postImageView setImage:self.userImage];
+    }];
 }
 
 - (void)textViewDidChange:(UITextView *)textView {
     // Show placeholder text when no caption has been entered
     if ([self.captionTextView hasText]) {
-        [self.placeholderLabel setHidden:NO];
-    } else {
         [self.placeholderLabel setHidden:YES];
+    } else {
+        [self.placeholderLabel setHidden:NO];
     }
 }
 
