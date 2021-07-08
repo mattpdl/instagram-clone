3658f59497aec034019ab151172375636688733e
Matthew John Ponce de Leon <mattpdl@fb.com>
2021-07-07 18:58:48 -0700

    Add image picker to compose view

---
+++ Instagram/ComposeViewController.h
@@ -0,0 +1,16 @@
+//
+//  ComposeViewController.h
+//  Instagram
+//
+//  Created by mattpdl on 7/7/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ComposeViewController : UIViewController <UITableViewDelegate, UITableViewDataSource, UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+
+@end
+
+NS_ASSUME_NONNULL_END
